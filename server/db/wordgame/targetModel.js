373f799890f182758fa1d7ab36b1a233612bbe79
Kevin J Urban <motocru@users.noreply.github.com>
2019-08-01 06:50:35 -0500

    removing unneeded target model

--- server/db/wordgame/targetModel.js
+++
@@ -1,8 +0,0 @@
-/**This file serves as a model for the target object containing
- * the answer for the related game.
- */
-function Target(id, answer) {
-  this.id = id;
-  this.answer = answer
-}
-module.exports = Target;
