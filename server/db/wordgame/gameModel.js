3a756f6ac09fa8f4d89f5bc443558f1ecb0df6eb
Kevin J Urban <motocru@users.noreply.github.com>
2019-08-01 06:50:18 -0500

    removing unneeded gamemodel

--- server/db/wordgame/gameModel.js
+++
@@ -1,8 +0,0 @@
-/**this file serves as a model for the grammarguru games */
-function Game(userId, colors, font, level) {
-  this.userId = userId;
-  this.colors = colors;
-  this.font = font;
-  this.level = level;
-}
-module.exports = Game;
