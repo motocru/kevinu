4a2a8e0b8f9b426ddfb34374020432f4dc0923f7
Kevin Urban <kurban@reconyx.com>
2025-11-28 12:06:07 -0600

    WIP

--- redesign/src/routes/+page.svelte
+++ redesign/src/routes/+page.svelte
@@ -11,29 +11,31 @@
 	});
 </script>
 
-<section id="main-header" class="section">
-	{#if ready}
-		<div class="split-screen">
-			<div class="pane">
-				<h1 in:fly={{ y: 100, duration: 1000 }}>Kevin Urban</h1>
-				<h2 in:fly={{ y: 100, duration: 1800 }}>Full Stack Developer</h2>
-				<p in:fly={{ y: 100, duration: 2800 }}>
-					My name is Kevin Urban and I am a full stack developer with a passion for creating
-					innovative and user-friendly web applications.
-				</p>
+<section id="home" class="section">
+	<div class="section-content">
+		{#if ready}
+			<div class="split-screen">
+				<div class="pane">
+					<h1 in:fly={{ y: 100, duration: 1000 }}>Kevin Urban</h1>
+					<h2 in:fly={{ y: 100, duration: 1800 }}>Full Stack Developer</h2>
+					<p in:fly={{ y: 100, duration: 2800 }}>
+						My name is Kevin Urban and I am a full stack developer with a passion for creating
+						innovative and user-friendly web applications.
+					</p>
+				</div>
+				<div class="pane">
+					<button
+						in:fly={{ y: 100, duration: 600, delay: 1000 }}
+						class="view-work-button"
+						type="button"
+						onclick={() => {
+							goto('#about');
+						}}>View my work <span style="font-size: 18px;">↓</span></button
+					>
+				</div>
 			</div>
-			<div class="pane">
-				<button
-					in:fly={{ y: 100, duration: 600, delay: 1000 }}
-					class="view-work-button"
-					type="button"
-					onclick={() => {
-						goto('#about');
-					}}>View my work <span style="font-size: 18px;">↓</span></button
-				>
-			</div>
-		</div>
-	{/if}
+		{/if}
+	</div>
 </section>
 
 <!-- Our main navigation bar. placed here to provide position for sticky -->
@@ -46,27 +48,48 @@
 	<div class="section-header">
 		<h1 class="text-5xl">About</h1>
 	</div>
-	<div class="split-screen">
-		<div class="pane">
-			<p>
-				A deeply passionate full stack and app developer with whom learning never stops. I have
-				experience in a wide range of technologies and frameworks, and I am always looking for new
-				opportunities to learn and grow. I also have experience with cloud services and deployment.
-				When not coding, I enjoy playing video games, reading books, hiking, snowboarding and
-				spending time with my wife and dog.
-			</p>
+
+	<div class="section-content pr-5">
+		<div class="split-screen">
+			<div class="pane">
+				<p>
+					A deeply passionate full stack and app developer with whom learning never stops. I have
+					experience in a wide range of technologies and frameworks, and I am always looking for new
+					opportunities to learn and grow. I also have experience with cloud services and
+					deployment. When not coding, I enjoy playing video games, reading books, hiking,
+					snowboarding and spending time with my wife and dog.
+				</p>
+			</div>
+			<div class="pane">
+				Some lorem ipsum text for all of this to test out the thing that I'm doing here.
+			</div>
 		</div>
-		<div class="pane"></div>
 	</div>
 </section>
-<section id="projects" class="section"></section>
-<section id="resume" class="section"></section>
-<section id="contact" class="section"></section>
+<section id="projects" class="section">
+	<div class="section-header">
+		<h1 class="text-5xl">Projects</h1>
+	</div>
+</section>
+<section id="resume" class="section">
+	<div class="section-header">
+		<h1 class="text-5xl">Resume</h1>
+	</div>
+</section>
+<section id="contact" class="section">
+	<div class="section-header">
+		<h1 class="text-5xl">Contact</h1>
+	</div>
+</section>
 
 <style lang="postcss">
 	.section {
-		height: 100vh;
+		min-height: 100vh;
 		width: 100%;
+	}
+
+	.section-content {
+		justify-content: center;
 	}
 
 	.split-screen {
@@ -109,7 +132,13 @@
 	}
 
 	.section-header {
+		position: relative;
 		padding-top: 5rem;
-		padding-left: 5rem;
+		margin-bottom: 0;
+		padding-bottom: 0;
+	}
+
+	.section-header h1 {
+		margin: 0;
 	}
 </style>
