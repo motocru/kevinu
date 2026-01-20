55c3afe2474e735cba631712a397e0998dece084
Kevin Urban <kurban@reconyx.com>
2026-01-19 21:25:13 -0600

    Worked on getting the timer setup

--- redesign-next/app/projects/timer/page.tsx
+++ redesign-next/app/projects/timer/page.tsx
@@ -8,13 +8,24 @@ interface TimerGame {
 
 }
 
+enum Game {
+    QuakeLive,
+    QuakeChampions
+}
+
 export default function Timer() {
     const [currentGame, setCurrentGame] = useState<TimerGame | null>(null);
     const [playerId] = useState(() => crypto.randomUUID());
-    const [selectedGame, setSelectedGame] = useState("Quake Live");
-    const gameOptions = ["Quake Live", "Quake Champions"];
+    const [selectedGame, setSelectedGame] = useState(Game.QuakeLive);
     const [rounds, setRounds] = useState(5);
-    const roundsOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const [megaHealth, setMegaHealth] = useState(false);
+    const [heavyArmor, setHeavyArmor] = useState(false);
+
+    const roundsOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    const gameRecord: Record<string, Game> = {
+        "Quake Live": Game.QuakeLive,
+        "Quake Champions": Game.QuakeChampions
+    };
     return (
         <div>
             <h1>Timer</h1>
@@ -23,12 +34,13 @@ export default function Timer() {
                 <div className="timer-settings">
                     <div className="game-selection">
                         <label>Game:</label>
-                        <select className="select-input" value={selectedGame} onChange={(e) => setSelectedGame(e.target.value)}>
-                            {gameOptions.map((game) => (
-                                <option key={game} value={game}>{game}</option>
+                        <select className="select-input" value={selectedGame} onChange={(e) => { setSelectedGame(Number(e.target.value)) }}>
+                            {Object.entries(gameRecord).map(([key, value]) => (
+                                <option key={key} value={value}>{key}</option>
                             ))}
                         </select>
                     </div>
+                    |
                     <div className="rounds-selection">
                         <label>Rounds:</label>
                         <select className="select-input" value={rounds} onChange={(e) => setRounds(Number(e.target.value))}>
@@ -40,9 +52,21 @@ export default function Timer() {
                 </div>
                 <div className="timer-settings">
                     {/* TODO: Add items to be timed */}
+                    <div className="item-selection">
+                        <label>Item(s):</label>
+                        <input type="checkbox" id="megaHealth" name="megaHealth" value="Mega Health" onChange={(e) => setMegaHealth(e.target.checked)} />
+                        <label htmlFor="megaHealth">Mega Health<p>(35 seconds)</p></label>
+                        {selectedGame === Game.QuakeLive && (
+                            <div className="item-selection">
+                                <input type="checkbox" id="heavyArmor" name="heavyArmor" value="Heavy Armor" onChange={(e) => setHeavyArmor(e.target.checked)} />
+                                <label htmlFor="heavyArmor">Heavy Armor<p>(25 seconds)</p></label>
+                            </div>
+                        )}
+                        <button className="new-game-button">New Game</button>
+                    </div>
                 </div>
-                <div className="timer-settings">
-                    {/* TODO: Add start button */}
+                <div className="timer-settings pt-2">
+
                 </div>
                 <div className="timer-game">
                     {/* TODO: Add game area */}
