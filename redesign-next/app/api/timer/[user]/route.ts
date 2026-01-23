830112ab74cb9f1271349886de4163fcfae4ae66
Kevin Urban <urban.kevi@gmail.com>
2026-01-23 15:04:01 -0600

    including missed import changes

--- redesign-next/app/api/timer/[user]/route.ts
+++ redesign-next/app/api/timer/[user]/route.ts
@@ -4,6 +4,7 @@ import { TimerGame, TimerRound, TimerFullGame } from "@/server/timer/objects";
 //GET all of the timer game results for the user
 export async function GET(request: Request, { params }: { params: Promise<{ user: string }> }) {
     const { user } = await params;
+
     return new Response(`Hello ${user}`);
 }
 
