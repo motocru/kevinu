1adc50fcf74c2a3d38fae69e541e362ba347264f
Kevin Urban <urban.kevi@gmail.com>
2026-02-05 07:18:46 -0600

    WIP

--- redesign-next/app/api/timer/[user]/[round]/route.ts
+++ redesign-next/app/api/timer/[user]/[round]/route.ts
@@ -3,8 +3,10 @@ import { NextResponse } from "next/server";
 export async function PUT(request: Request, { params }: { params: Promise<{ user: string, round: string }> }) {
     const { user, round } = await params;
     const { time } = await request.json() as { time: number };
+
     if (!time || time > 59 || time < 0) {
         return NextResponse.json({ error: "Time is required and must be between 0 and 59" }, { status: 400 });
     }
+
     return new Response(`Hello ${user} ${round}`);
 }
