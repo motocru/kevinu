6a39845a6e38f35b7a4fef0fc994d2da44bd69db
Kevin Urban <urban.kevi@gmail.com>
2026-02-14 13:21:35 -0600

    docker compose and database connection finalized

--- server/db.ts
+++ server/db.ts
@@ -2,9 +2,9 @@ import mysql, { ResultSetHeader } from "mysql2";
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
